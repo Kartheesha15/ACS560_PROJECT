e3aa33c189e3742095482cacb26c59ce5f9edee6
HarshithaGudapati <gudapatiharshitha8247@gmail.com>
2024-10-22 03:07:42 +0000

    model impl

---
+++ currencyconverter/src/model/CurrencyConversion.java
@@ -0,0 +1,42 @@
+package com.example.currencyconverter.model;
+
+public class CurrencyConversion {
+    private double amount;
+    private String fromCurrency;
+    private String toCurrency;
+    private double convertedAmount;
+
+    // Getters and Setters
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getFromCurrency() {
+        return fromCurrency;
+    }
+
+    public void setFromCurrency(String fromCurrency) {
+        this.fromCurrency = fromCurrency;
+    }
+
+    public String getToCurrency() {
+        return toCurrency;
+    }
+
+    public void setToCurrency(String toCurrency) {
+        this.toCurrency = toCurrency;
+    }
+
+    public double getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public void setConvertedAmount(double convertedAmount) {
+        this.convertedAmount = convertedAmount;
+    }
+
+}
