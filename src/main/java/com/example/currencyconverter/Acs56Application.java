1d7735e22e0ae75b8b5336c4772bead5eaffa19a
kartheesha <kartheesha15@gmail.com>
2024-10-24 17:02:35 -0400

    project

---
+++ src/main/java/com/example/currencyconverter/Acs56Application.java
@@ -0,0 +1,13 @@
+package com.example.currencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Acs56Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Acs56Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/acs56/Acs56ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acs56;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Acs56ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
